4c5bec0910383ddfd5af92dccc12c9dde74c9d08
Samuel O Rodriguez <SamuelORodriguez@users.noreply.github.com>
2018-09-20 21:45:29 -0400

    Add files via upload

---
+++ MegaBytesConverter.java
@@ -0,0 +1,15 @@
+package com.timbuchalka;
+
+public class MegaBytesConverter {
+	public static void main(String[] args){
+		
+		  public static printMegaBytesAndKiloBytes(int kiloBytes) {
+		      if ( kiloBytes < 0) {
+				          System.out.println("Invalid Value");
+				      }else{
+				            System.out.println(kiloBytes + "KB = " + kiloBytes/1024 + " MB and " + kiloBytes%1024 + " KB");
+				      }
+		      return kiloBytes;
+		  }
+	}
+}
